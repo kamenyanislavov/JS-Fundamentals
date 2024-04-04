e7ed51539ff18a6e0f222b8ea702fd5ab9ebeea7
kamenyanislavov <kamishon@gmail.com>
2024-04-04 23:23:56 +0300

    upload homeworks JS fund

--- basicSyntax_condStat_loops/ages.js
+++ basicSyntax_condStat_loops/ages.js
@@ -20,11 +20,6 @@ function ages(age) {
     }
 }
 
-// ·	0-2 (age) – is a baby;   
-// ·	3-13 (age) – is a child; 
-// ·	14-19 (age) – is a teenager;
-// ·	20-65 (age) – is an adult;
-// ·	>=66 (age) – is an elder; 
 
 ages(20)
 ages(5)
---
+++ basicSyntax_condStat_loops/bitcoinMining.js
@@ -0,0 +1,50 @@
+function bicoinMining(input) {
+
+    let bitcoinPrice = 11949.16;  // price per piece
+    let goldPrice = 67.51;  // price per gram
+    let counter = 0;
+    let day = 0;
+    let bitcoins = 0;
+    let gold = 0;
+    let total_gold = 0;
+    let money = 0;
+
+    // dig the gold during the shift
+    for (let i = 0; i < input.length; i++) {
+        gold = input[i];
+        counter++
+        // check if we have enough money for the first bitcoin
+
+        if (counter % 3 == 0) {
+            total_gold += gold * 0.70;
+        } else {
+            total_gold += gold;
+        }
+
+        // convert the gold for money
+        money = total_gold * goldPrice;
+
+        while (bitcoins == 0) {
+            day++
+            break;
+        }
+        bitcoins = Math.floor(money / bitcoinPrice);
+        money -= bitcoins * bitcoinPrice;
+    }
+
+    // buy the bitcoins and calculate the rest of the money
+
+
+    console.log(`Bought bitcoins: ${bitcoins}`);
+    if (bitcoins > 0) {
+        console.log(`Day of the first purchased bitcoin: ${day}`);
+    }
+    console.log(`Left money: ${money.toFixed(2)} lv.`);
+
+}
+
+bicoinMining([100, 200, 300]);
+console.log('------------------');
+bicoinMining([50, 100]);
+console.log('------------------');
+bicoinMining([3124.15, 504.212, 2511.124]);
