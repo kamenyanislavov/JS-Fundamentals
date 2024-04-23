27d5ea152675f1e9261e061d5bafe9cd67f836b2
kamenyanislavov <kamishon@gmail.com>
2024-04-23 06:11:56 +0300

    js fund homework upload

---
+++ dataTypesAndVariables/reversedChars.js
@@ -0,0 +1,15 @@
+function reversedChars(p1, p2, p3) {
+
+    console.log(`${p3} ${p2} ${p1}`);
+    
+}
+
+reversedChars('A',
+'B',
+'C'
+);
+console.log('-----------');
+reversedChars('1',
+'L',
+'&'
+);
