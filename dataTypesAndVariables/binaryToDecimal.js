f2bbeaf173752bfe4d3c7c1cf8b40a342c11c801
kamenyanislavov <kamishon@gmail.com>
2024-05-23 07:24:38 +0300

    js fund exercise

--- dataTypesAndVariables/binaryToDecimal.js
+++ dataTypesAndVariables/binaryToDecimal.js
@@ -1,9 +1,15 @@
 function binaryToDecimal(binary) {
 
-    console.log(parseInt(binary, 2));
+    let decimal = 0;
 
+    for (let i = 0; i < binary.length; i++) {
+        decimal += Number(binary[i]) * (2 ** (binary.length - 1 - i));
+    }
+
+    return decimal;
 
 }
 
 binaryToDecimal('00001001');
+console.log('---------------');
 binaryToDecimal('11110000');
