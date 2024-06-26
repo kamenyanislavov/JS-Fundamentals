522b939fd0cf9810c28a4aae6fa88bf06915fb63
kamenyanislavov <kamishon@gmail.com>
2024-06-26 06:27:55 +0300

    js fund arrays exercise

--- arraysAdvanced/demoArrays.js
+++ arraysAdvanced/demoArrays.js
@@ -8,5 +8,32 @@ let numbers = [];
 array1.unshift(array0);
 numbers.push(array1, array2, array3);
 
-console.log(numbers[0][0][0]);
-console.log(numbers[2][2] + numbers[0][0][0]);
+// console.log(numbers[0][0][0]);
+// console.log(numbers[2][2] + numbers[0][0][0]);
+
+
+
+const array = [14, 12, 44, 53, 21];
+
+const index = 2;
+const step = 5;
+let newIndex = index;
+
+if (index - step >= 0) {
+    newIndex = index - step;
+} else {
+    for (let i = 1; i <= step; i++) {
+
+        if (newIndex == 0) {
+            newIndex = array.length - 1;
+        } else {
+            newIndex -= 1;
+        
+        }
+    }
+}
+console.log(newIndex);
+
+
+
+
