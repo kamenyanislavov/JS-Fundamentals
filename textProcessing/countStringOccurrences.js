cb17f4ad55b97073b347fda0d88f64e307556e1e
kamenyanislavov <kamishon@gmail.com>
2024-07-11 21:14:26 +0300

    js fund exercise text processing

--- textProcessing/countStringOccurrences.js
+++ textProcessing/countStringOccurrences.js
@@ -1,7 +1,15 @@
 function countWordOccurrence(text, word) {
 
-    const 
+    const textAsArray = text.split(" ");
+    let counter = 0;
 
+    for (let el of textAsArray) {
+        if (el == word) {
+            counter++
+        }
+    }
+
+    console.log(counter);
 }
 
 countWordOccurrence('This is a word and it also is a sentence',
